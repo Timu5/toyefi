465285aed77d109bb459cfff499a4d4a3874f4c9
Timu5 <timusziomek@gmail.com>
2019-07-17 18:43:18 +0200

    Add comments to engine.py

--- engine.py
+++ engine.py
@@ -3,25 +3,25 @@ from lookup import *
 class Engine:
 	
 	def __init__(self):
-		self.disp = 500
-		self.injector_flow = 315
+		self.disp = 500 # cc
+		self.injector_flow = 315 # cc/min
 		self.injector_deadtime = 1 # ms
 		
 		self.table_ve = Table2D()
 		self.table_warm = Table1D()
 		
-		self.map = 1
-		self.iat = 22
-		self.cht = 90
-		self.o2 = 1
+		self.map = 1 # bar
+		self.iat = 22 # °C
+		self.cht = 90 # °C
+		self.o2 = 1 # lamda
 		
 		self.ve = 0.8
 		self.afr = 14.7
-		self.warm = 1
-		self.pw = 0
+		self.warm = 1 # warmup enrichment
+		self.pw = 0 # injector puslewidth
 		
 		self.rpm = 0
-		self.adv = 0
+		self.adv = 0 # ignition advance
 		
 	def read_adc(self):
 		# in real implementation we would sample each adc channel
