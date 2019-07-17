aacbddbfd5eb31f33e8356a8d5adadd16e256742
Timu5 <timusziomek@gmail.com>
2019-07-17 23:32:16 +0200

    Add comments

--- engine.py
+++ engine.py
@@ -3,25 +3,28 @@ from lookup import *
 class Engine:
 	
 	def __init__(self):
+		# const
 		self.disp = 500 # cc
 		self.injector_flow = 315 # cc/min
 		self.injector_deadtime = 1 # ms
 		
+		# tables
 		self.table_ve = Table2D()
 		self.table_warm = Table1D()
 		self.table_ign = Table2D()
 		
+		# data from sensors
 		self.map = 1 # bar
 		self.iat = 22 # °C
 		self.cht = 90 # °C
 		self.o2 = 1 # lamda
 		
+		# calculated variables
+		self.rpm = 1000
 		self.ve = 0.8
 		self.afr = 14.7
 		self.warm = 1 # warmup enrichment
 		self.pw = 0 # injector puslewidth
-		
-		self.rpm = 0
 		self.adv = 0 # ignition advance
 		
 	def read_adc(self):
